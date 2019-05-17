b596402707067d50ad02008ccf30a6f037f24e0d
盛领宜 <sheng.ly_wh@www.sand.com>
2019-05-17 09:18:33 +0800

    提交

--- src/main/java/com/sly/plugin/antiduplicatecommit/configuration/AntiDuplicateCommitConfig.java
+++ src/main/java/com/sly/plugin/antiduplicatecommit/configuration/AntiDuplicateCommitConfig.java
@@ -2,7 +2,6 @@ package com.sly.plugin.antiduplicatecommit.configuration;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.sly.plugin.antiduplicatecommit.aop.AntiDuplicateCommitAspect;
@@ -14,7 +13,6 @@ import com.sly.plugin.antiduplicatecommit.properties.AntiDuplicateCommitProperti
  * @time 2019年5月16日
  */
 @Configuration
-@ComponentScan(basePackages = "com.sly.plugin.antiduplicatecommit.aop")
 @EnableConfigurationProperties(AntiDuplicateCommitProperties.class)
 public class AntiDuplicateCommitConfig {
 	
